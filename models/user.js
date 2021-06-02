a1fe55e87c92185def4bdbcd7cb2de8f36560333
Voropay Dmitriy <t79508608460@yandex.ru>
2021-06-02 21:59:40 +0300

    fix: использовать throw вместо reject

--- models/user.js
+++ models/user.js
@@ -27,17 +27,13 @@ const userSchema = new mongoose.Schema({
 
 userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email }).select('+password')
+    .orFail(()=> {throw new CredentialsError('Неправильные почта или пароль')})
     .then((user) => {
-      if (!user) {
-        return Promise.reject(new CredentialsError('Неправильные почта или пароль'));
-      }
-
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            return Promise.reject(new CredentialsError('Неправильные почта или пароль'));
+            throw new CredentialsError('Неправильные почта или пароль');
           }
-
           return user;
         });
     });
