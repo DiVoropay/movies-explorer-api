0e233a8e031b619b40459708f132d2f3194ec5fe
Voropay Dmitriy <t79508608460@yandex.ru>
2021-06-02 22:57:25 +0300

    feat: добавить централизованный оработчик ошибок

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 
+const handlerErrors = require('./middliwares/handler-errors');
+
 const { PORT = 3000 } = process.env;
 
 const app = express();
@@ -21,6 +23,8 @@ mongoose.connect('mongodb://localhost:27017/moviesexplorerdb', {
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', require('./routes'));
+
+app.use(handlerErrors);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const { createUser, login } = require('../controllers/users');
 
-router.get('/singup', createUser);
-router.patch('/signin', login);
+router.post('/signup', createUser);
+router.post('/signin', login);
 
 module.exports = router;
