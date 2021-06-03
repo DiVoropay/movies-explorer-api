c352e441f80ddb1a88974df0bef337677e2f0fcd
Voropay Dmitriy <t79508608460@yandex.ru>
2021-06-03 23:23:33 +0300

    feat: добавить мидлвар авт-ции и запись user в req

---
+++ middliwares/auth.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+const jwt = require('jsonwebtoken');
+
+const { NODE_ENV, JWT_SECRET_KEY } = process.env;
+
+const CredentialsError = require('../errors/credentials-error');
+
+const extractBearerToken = (header) => header.replace('Bearer ', '');
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new CredentialsError('Необходима авторизация');
+  }
+
+  const token = extractBearerToken(authorization);
+  let payload;
+
+  try {
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET_KEY : 'movies-secret');
+  } catch (err) {
+    throw new CredentialsError('Необходима авторизация');
+  }
+
+  req.user = payload;
+
+  next();
+};
