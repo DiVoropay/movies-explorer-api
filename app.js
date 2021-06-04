a5cd624ae5094e98ab169e09c6d3908c2619c63f
Voropay Dmitriy <t79508608460@yandex.ru>
2021-06-05 00:33:42 +0300

    feat: добавить логер

--- app.js
+++ app.js
@@ -4,14 +4,12 @@ const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 
-const handlerErrors = require('./middliwares/handler-errors');
+const handlerErrors = require('./middlewares/handler-errors');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000, MONGO_DB = 'mongodb://localhost:27017/moviesexplorerdb' } = process.env;
 
 const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect(MONGO_DB, {
   useNewUrlParser: true,
@@ -20,10 +18,15 @@ mongoose.connect(MONGO_DB, {
   useUnifiedTopology: true,
 });
 
+app.use(requestLogger);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./routes'));
 
+app.use(errorLogger);
 app.use(handlerErrors);
 
 app.listen(PORT, () => {});
