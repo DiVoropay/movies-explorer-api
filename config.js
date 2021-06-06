13ff8d6d946d5ffd9e722d2dd4bf181279f28feb
Voropay Dmitriy <t79508608460@yandex.ru>
2021-06-07 01:58:50 +0300

    feat: учесть продакшн mongo базу

--- config.js
+++ config.js
@@ -1,16 +1,19 @@
 require('dotenv').config();
 
-const {
-  PORT = 3000,
-  MONGO_DB = 'mongodb://localhost:27017/moviesexplorerdb',
-  NODE_ENV,
-  JWT_SECRET_KEY,
-  JWT_EXPIRES_IN = '7d',
-} = process.env;
-
-module.exports = {
-  PORT,
-  MONGO_DB,
-  JWT_SECRET_KEY: NODE_ENV === 'production' ? JWT_SECRET_KEY : 'movies-secret',
-  JWT_EXPIRES_IN,
+const configDev = {
+  PORT: 3000,
+  MONGO_DB: 'mongodb://localhost:27017/moviesexplorerdb',
+  JWT_SECRET_KEY: 'movies-secret',
+  JWT_EXPIRES_IN: '7d',
 };
+
+const configProd = {
+  PORT: process.env.PORT || configDev.PORT,
+  MONGO_DB: process.env.MONGO_DB || configDev.MONGO_DB,
+  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY || configDev.JWT_SECRET_KEY,
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || configDev.JWT_EXPIRES_IN,
+};
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = isProduction ? configProd : configDev;
